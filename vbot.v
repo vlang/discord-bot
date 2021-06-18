14fc41c4f3d626856fbe7e18c450bfea23d8047b
shadowninja55 <shadowninja1050@gmail.com>
2021-06-17 21:05:19 -0400

    separate authorized roles to env var

--- vbot.v
+++ vbot.v
@@ -8,10 +8,11 @@ import json
 import regex
 
 const (
-	bot_token = os.getenv('VBOT_TOKEN')
-	vexeroot = @VEXEROOT
-	block_size = 4096
-	inode_ratio = 16384
+	bot_token        = os.getenv('VBOT_TOKEN')
+	authorized_roles = os.getenv('AUTHORIZED_ROLES').split(' ')
+	vexeroot         = @VEXEROOT
+	block_size       = 4096
+	inode_ratio      = 16384
 )
 
 struct State {
@@ -19,7 +20,7 @@ struct State {
 }
 
 fn main() {
-	os.execute("isolate --cleanup")
+	os.execute('isolate --cleanup')
 
 	mut client := discord.new(token: bot_token) ?
 	client.userdata = &State{load_docs_headers()}
@@ -55,21 +56,21 @@ fn sanitize(argument string) ? {
 	for letter in argument {
 		match letter {
 			`0`...`9`, `a`...`z`, `A`...`Z`, `.`, `_` {}
-			else { return error("Illegal character.") }
+			else { return error('Illegal character.') }
 		}
 	}
 }
 
 struct Section {
-	name string
-	content string
+	name     string
+	content  string
 	comments []string
 }
 
 fn vlib_command(options [][]string) string {
 	vlib_module := options[0][1]
 	query := options[1][1]
-	
+
 	sanitize(vlib_module) or {
 		return '{"content": "Only letters, numbers, ., and _ are allowed in module names."}'
 	}
@@ -93,29 +94,29 @@ fn vlib_command(options [][]string) string {
 
 	for section_ in sections {
 		section := section_.as_map()
-		name := section["name"].str()
+		name := section['name'].str()
 		score := strings.levenshtein_distance(query, name)
 
 		if score < lowest {
 			lowest = score
-			closest = Section {
+			closest = Section{
 				name: name
-				content: section["content"].json_str()
-				comments: section["comments"].arr().map(it.as_map()["text"].json_str())
+				content: section['content'].json_str()
+				comments: section['comments'].arr().map(it.as_map()['text'].json_str())
 			}
 		}
 
-		for child_ in section["children"].arr() {
+		for child_ in section['children'].arr() {
 			child := child_.as_map()
-			child_name := child["name"].str()
+			child_name := child['name'].str()
 			child_score := strings.levenshtein_distance(query, child_name)
 
 			if child_score < lowest {
 				lowest = child_score
-				closest = Section {
+				closest = Section{
 					name: child_name
-					content: child["content"].json_str()
-					comments: child["comments"].arr().map(it.as_map()["text"].json_str())
+					content: child['content'].json_str()
+					comments: child['comments'].arr().map(it.as_map()['text'].json_str())
 				}
 			}
 		}
@@ -125,7 +126,7 @@ fn vlib_command(options [][]string) string {
 	mut blob := ''
 
 	for comment in closest.comments {
-		blob += comment.trim_left("\u0001")
+		blob += comment.trim_left('\u0001')
 	}
 
 	if blob != '' {
@@ -175,55 +176,56 @@ fn docs_command(options [][]string, headers []string) string {
 
 fn on_message(mut client discord.Client, message &discord.Message) {
 	content := message.content
-	mut re := regex.regex_opt(r"/run\s+```[a-z]*\s+(.*)```") or { panic(err.msg) }
+	mut re := regex.regex_opt(r'/run\s+```[a-z]*\s+(.*)```') or { panic(err.msg) }
 	start, _ := re.match_string(content)
-	
+
 	if start != -1 {
 		roles := message.member.roles
 		mut authorized := false
 
-		// v-dev, moderator, owner
-		for role in ["671405628023111731", "592107317705703427", "595009999227453444"] {
+		for role in authorized_roles {
 			if role in roles {
 				authorized = true
 			}
 		}
 
 		if !authorized {
-			client.channel_message_send(message.channel_id, content: "You aren't authorized to use eval.") or {}
+			client.channel_message_send(message.channel_id,
+				content: "You aren't authorized to use eval."
+			) or {}
 			return
 		}
-		
+
 		group := re.get_group_list()[0]
 		code := content[group.start..group.end]
-		resp := "```\n${run_in_sandbox(code)}\n```"
+		resp := '```\n${run_in_sandbox(code)}\n```'
 		client.channel_message_send(message.channel_id, content: resp) or {}
 	}
 }
 
 fn run_in_sandbox(code string) string {
-	iso_res := os.execute("isolate --init")
-	defer { 
-		os.execute("isolate --cleanup") 
+	iso_res := os.execute('isolate --init')
+	defer {
+		os.execute('isolate --cleanup')
 	}
-	box_path := os.join_path(iso_res.output.trim_suffix("\n"), "box")
-	os.write_file(os.join_path(box_path, "code.v"), code) or {
-		return "Failed to write code to sandbox."
+	box_path := os.join_path(iso_res.output.trim_suffix('\n'), 'box')
+	os.write_file(os.join_path(box_path, 'code.v'), code) or {
+		return 'Failed to write code to sandbox.'
 	}
-	run_res := os.execute("isolate --dir=$vexeroot --env=HOME=/box --processes=3 --mem=100000 --wall-time=5 --quota=${1048576 / block_size},${1048576 / inode_ratio} --run $vexeroot/v run code.v")
+	run_res := os.execute('isolate --dir=$vexeroot --env=HOME=/box --processes=3 --mem=100000 --wall-time=5 --quota=${1048576 / block_size},${1048576 / inode_ratio} --run $vexeroot/v run code.v')
 	return prettify(run_res.output)
 }
 
-fn prettify(output string) string  {
+fn prettify(output string) string {
 	mut pretty := output
 
 	if pretty.len > 1992 {
-		pretty = pretty[..1989] + "..."
+		pretty = pretty[..1989] + '...'
 	}
 
-	nlines := pretty.count("\n")
+	nlines := pretty.count('\n')
 	if nlines > 5 {
-		pretty = pretty.split_into_lines()[..5].join_lines() + "\n...and ${nlines - 5} more"
+		pretty = pretty.split_into_lines()[..5].join_lines() + '\n...and ${nlines - 5} more'
 	}
 
 	return pretty
